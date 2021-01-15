7cd76c192ac100aa33e2b0d3cee589a3bf162b41
SiyanaNova <75304865+SiyanaNova@users.noreply.github.com>
2021-01-15 12:28:37 +0000

    added "last updated" to JS

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-function formatDate(date) {
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
   
-    let dayIndex = date.getDay();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let day = days[dayIndex];
-  
-    return `${day} ${hours}:${minutes}`;
-  }
+let today = new Date();
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+let day = days[today.getDay()];
+let hour= today.getHours();
+let minute= today.getMinutes();
+
+let date = document.querySelector("#date");
+date.innerHTML= ` ${day}, ${hour}:${minute}`;
   
   function displayWeatherCondition(response) {
     document.querySelector("#city").innerHTML = response.data.name;
