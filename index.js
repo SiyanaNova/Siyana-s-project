312af90ba196665230562632a0699999cbe076e3
SiyanaNova <75304865+SiyanaNova@users.noreply.github.com>
2021-01-24 18:17:07 +0000

    updated the forecast styling

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ if (minute < 10) {
   <h3>${formatHours(forecast.dt*1000)}</h3>
   <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" class="hourly"/>
   <div class="weather-forecast-temperature">
-   <strong>${Math.round(forecast.main.temp_max)}°</strong>${Math.round(forecast.main.temp_min)}°
+   <strong>${Math.round(forecast.main.temp_max)}°</strong>/${Math.round(forecast.main.temp_min)}°
   </div>
   </div> 
 </div>
